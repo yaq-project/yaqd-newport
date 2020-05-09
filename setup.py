8e231f56b1ba20cb1aacf401da59b17c2f82c2fe
Kyle Sunden <git@ksunden.space>
2020-05-08 20:22:32 -0500

    Update setup.py for aserial usage

--- setup.py
+++ setup.py
@@ -21,14 +21,19 @@ setup(
     packages=find_packages(exclude=("tests", "tests.*")),
     package_data=extra_files,
     python_requires=">=3.7",
-    install_requires=["yaqd-core", "yaq-serial"],
+    install_requires=["yaqd-core[aserial]"],
     extras_require={"dev": ["black", "pre-commit", "pydocstyle"],},
     version=version,
-    description="Core structures for yaq component daemons",
-    # long_description=read("README.rst"),
+    description="yaq daemons for Newport hardware",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     author="yaq Developers",
     license="LGPL v3",
     url="http://gitlab.com/yaq/yaqd-newport",
+    project_urls={
+        "Source": "https://gitlab.com/yaq/yaqd-newport",
+        "Issue Tracker": "https://gitlab.com/yaq/yaqd-newport/issues",
+    },
     entry_points={
         "console_scripts": [
             "yaqd-newport-smc100=yaqd_newport._newport_smc100:NewportSMC100.main",
@@ -37,12 +42,13 @@ setup(
     },
     keywords="spectroscopy science multidimensional hardware",
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering",
     ],
 )
