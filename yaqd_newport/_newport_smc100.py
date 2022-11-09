6ef8a7b348fe92efb18c2bb843cfc676580565d8
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2022-11-09 13:45:01 -0600

    native units
    correctly implement native_units (need to declare after trait init)

--- yaqd_newport/_newport_smc100.py
+++ yaqd_newport/_newport_smc100.py
@@ -3,3 +3,7 @@ from ._newport_motor import NewportMotor
 
 class NewportSMC100(NewportMotor):
     _kind = "newport-smc100"
+
+    def __init__(self, name, config, config_filepath):
+        super().__init__(name, config, config_filepath)
+        self._native_units = "mm"
