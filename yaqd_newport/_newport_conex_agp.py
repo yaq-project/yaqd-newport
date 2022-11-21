0fc7728759b70e69b50fe5a447e683e7da704e61
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2022-11-21 10:55:40 -0600

    bump version, native units (#13)
    * Update _newport_conex_agp.py
    
    * native units
    
    correctly implement native_units (need to declare after trait init)
    
    * bump version

--- yaqd_newport/_newport_conex_agp.py
+++ yaqd_newport/_newport_conex_agp.py
@@ -3,3 +3,7 @@ from ._newport_motor import NewportMotor
 
 class NewportConexAGP(NewportMotor):
     _kind = "newport-conex-agp"
+
+    def __init__(self, name, config, config_filepath):
+        super().__init__(name, config, config_filepath)
+        self._native_units = "deg"
