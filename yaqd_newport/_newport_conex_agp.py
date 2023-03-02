1aa784314fe4e37e1b1d46cd8cf26ba695396818
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-03-02 12:31:06 -0600

    Update _newport_conex_agp.py

--- yaqd_newport/_newport_conex_agp.py
+++ yaqd_newport/_newport_conex_agp.py
@@ -7,3 +7,11 @@ class NewportConexAGP(NewportMotor):
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
         self._native_units = "deg"
+
+    async def save_state(self):
+        try:
+            self.logger.info(f"Will I write to disk? {self._state.updated}")
+            await super().save_state()
+        except Exception as e:
+            self.logger.error(str(e))
+            raise e
