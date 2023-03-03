5eec9cae1aaf3c823c4898a4d97ea9aca8167f53
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-03-03 13:50:15 -0600

    Update _newport_conex_agp.py

--- yaqd_newport/_newport_conex_agp.py
+++ yaqd_newport/_newport_conex_agp.py
@@ -8,19 +8,19 @@ class NewportConexAGP(NewportMotor):
         super().__init__(name, config, config_filepath)
         self._native_units = "deg"
 
-    def _save_state(self):
-        try:
-            self.logger.info(f"Will I write to disk? {self._state.updated}")
-            super()._save_state()
-        except Exception as e:
-            self.logger.error(str(e))
-            raise e
+    # def _save_state(self):
+    #     try:
+    #         self.logger.info(f"Will I write to disk? {self._state.updated}")
+    #         super()._save_state()
+    #     except Exception as e:
+    #         self.logger.error(str(e))
+    #         raise e
 
-    async def update_state(self):
-        try:
-            self.logger.info(f"updating state...")
-            await super().update_state()
-        except Exception as e:
-            self.logger.error(str(e))
-            raise e
+    # async def update_state(self):
+    #     try:
+    #         self.logger.info(f"updating state...")
+    #         await super().update_state()
+    #     except Exception as e:
+    #         self.logger.error(str(e))
+    #         raise e
 
